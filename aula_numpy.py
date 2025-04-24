d51877bdfc1c09d89947589fe3c356a3c021c07f
Victória Carvalho <loonartpop@gmail.com>
2025-04-24 19:52:56 -0400

    Add files via upload

---
+++ aula_numpy.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array_letras = np.array(['a','b','c','d','e'])
+
+print(array_letras)
+
+#Acessando posições especificas
+
+print(array_letras[2])
+
+#Acessando partes do array
+print(array_letras[0:3])
---
+++ exer_1504.py
@@ -0,0 +1,5 @@
+slamano = {
+
+}
+
+print(slamano)
